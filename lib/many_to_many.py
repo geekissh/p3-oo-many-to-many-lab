e5c5d373335c0ad269afea99234c6d67f6a9355f
Mark Kelsall <markkelsall1995@gmail.com>
2024-03-22 07:22:17 +0300

    final

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,59 @@
 class Author:
-    pass
+    all = []
 
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def __repr__(self):
+        return f'<Author {self.name}>'
+
+    def contracts(self):
+        contracts = []
+        for contract in Contract.all:
+            if contract.author == self:
+                contracts.append(contract)
+        return contracts
+
+    def books(self):
+        books = []
+        for contract in self.contracts():
+            books.append(contract.book)
+        return books
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        return contract
+
+    def total_royalties(self):
+        total = 0
+        for contract in self.contracts():
+            total += contract.royalties
+        return total
 
 class Book:
-    pass
+    all = []
 
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    def __repr__(self):
+        return f'<Contract {self.date}>'
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
